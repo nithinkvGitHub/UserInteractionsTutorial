872ea8f7147c531bb56abcc49d71c1564f963358
Nithin <nithinkv@live.com>
2018-05-21 19:41:16 +0530

    evening

---
+++ UserInteractionsTutorial/Interactions.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace UserInteractionsTutorial
+{
+
+    [TestFixture]
+
+    public class InteractionDemo
+    {
+
+        [Test]
+
+        public void Example1()
+        {
+            var driver = new FirefoxDriver();
+            Actions action = new Actions(driver);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(6));
+
+            driver.Navigate().GoToUrl("http://jqueryui.com/droppable/");
+            wait.Until(ExpectedConditions.)
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
